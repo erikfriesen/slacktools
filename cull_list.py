9cb8858942909b11766b7fd14604d8dd1d4e0294
Erik Friesen <erik.friesen@gmail.com>
2017-04-12 17:56:10 -0500

    commented my damn code

--- cull_list.py
+++ cull_list.py
@@ -1,12 +1,16 @@
 from slacker import Slacker
 
+#grab api key
 f = open('config', 'r')
 apikey = f.read()
 
+#grab all channel details
 slack = Slacker(apikey)
 channel_all = slack.channels.list()
 channel_time = dict()
 
+# create dict (channel id as key)
+# containing channel name and timestamp of most recent post
 for i in channel_all.body['channels']:
     try:
         channel_time[i['id']] = [i['name'],
