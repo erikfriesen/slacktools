df72afa1cb684d85f32de2b13c2a4dabceadb302
Erik Friesen <erik.friesen@gmail.com>
2017-04-12 23:44:31 -0500

    filter out archived channels

--- cull_list.py
+++ cull_list.py
@@ -1,5 +1,5 @@
 from slacker import Slacker
-
+import arrow
 #grab api key
 f = open('config', 'r')
 apikey = f.read()
@@ -12,9 +12,9 @@ channel_time = dict()
 # create dict (channel id as key)
 # containing channel name and timestamp of most recent post
 for i in channel_all.body['channels']:
-    try:
-        channel_time[i['id']] = [i['name'],
-            slack.channels.history(i['id'], count=1).body['messages'][0]['ts']]
+    if i['is_archived'] is False:
+        try:
+            channel_time[i['id']] = [i['name'], arrow.get(slack.channels.history(i['id'], count=1).body['messages'][0]['ts'])]
 #handle empty channels    
-    except:
-        channel_time[i['id']] = [i['name'], 'none']
+        except:
+            channel_time[i['id']] = [i['name'], 'none']
