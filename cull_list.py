166416e8fa9ffd899bb971fc3b61baf47154f713
Erik Friesen <erik.friesen@gmail.com>
2017-04-19 19:38:06 -0500

    broke up code in TRY block to be more readable

--- cull_list.py
+++ cull_list.py
@@ -1,5 +1,6 @@
 from slacker import Slacker
 import arrow
+
 #grab api key
 f = open('config', 'r')
 apikey = f.read()
@@ -14,7 +15,9 @@ channel_time = dict()
 for i in channel_all.body['channels']:
     if i['is_archived'] is False:
         try:
-            channel_time[i['id']] = [i['name'], arrow.get(slack.channels.history(i['id'], count=1).body['messages'][0]['ts'])]
-#handle empty channels    
+            last_post = arrow.get(slack.channels.history(i['id'], count=1).body['messages'][0]['ts']).humanize()
+            channel_name = i['name']
+            channel_time[i['id']] = [channel_name, last_post]
+        #handle empty channels
         except:
             channel_time[i['id']] = [i['name'], 'none']
